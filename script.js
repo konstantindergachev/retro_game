df3dbb8461266d7bde2b395b3aeaf277880f2901
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2023-11-16 10:25:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+class Player {}
+class Projectile {}
+class Enemy {}
+class Game {}
+
+window.addEventListener('load', () => {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 600;
+  canvas.height = 800;
+});
