2c304f89200e031cf0876e8ef0b58dab070e05a4
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2023-11-16 10:34:45 +0000

    Created the game render method mockup

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 class Player {}
 class Projectile {}
 class Enemy {}
-class Game {}
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+  }
+
+  render() {
+    console.log(this.width, this.height);
+  }
+}
 
 window.addEventListener('load', () => {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
   canvas.width = 600;
   canvas.height = 800;
+
+  const game = new Game(canvas);
+  game.render();
 });
