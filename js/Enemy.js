4ee5bb5333789be92fe986701f93bbce7da38480
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2023-12-04 10:24:33 +0000

    Added the armored enemies

--- js/Enemy.js
+++ js/Enemy.js
@@ -70,3 +70,19 @@ export class Beetlemorph extends Enemy {
     this.maxLives = this.lives;
   }
 }
+
+export class Rhinomorph extends Enemy {
+  constructor(game, positionX, positionY) {
+    super(game, positionX, positionY);
+    this.image = document.getElementById('rhinomorph');
+    this.frameX = 0;
+    this.maxFrame = 5;
+    this.frameY = Math.floor(Math.random() * 4);
+    this.lives = 4;
+    this.maxLives = this.lives;
+  }
+  hit(damage) {
+    this.lives -= damage;
+    this.frameX = this.maxLives - Math.floor(this.lives);
+  }
+}
--- js/Wave.js
+++ js/Wave.js
@@ -1,4 +1,4 @@
-import { Beetlemorph } from './Enemy.js';
+import { Beetlemorph, Rhinomorph } from './Enemy.js';
 
 export class Wave {
   constructor(game) {
@@ -35,7 +35,11 @@ export class Wave {
       for (let x = 0; x < this.game.columns; x++) {
         let enemyX = x * this.game.enemySize;
         let enemyY = y * this.game.enemySize;
-        this.enemies.push(new Beetlemorph(this.game, enemyX, enemyY));
+        if (Math.random() < 0.5) {
+          this.enemies.push(new Rhinomorph(this.game, enemyX, enemyY));
+        } else {
+          this.enemies.push(new Beetlemorph(this.game, enemyX, enemyY));
+        }
       }
     }
   }
